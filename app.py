e0a323e4ff9c33ccaa2be1ee021d95b513102a70
Carlos Antunes <carlosantunes.dev@gmail.com>
2025-12-12 15:50:44 -0300

    Configuração completa do CRUD do modelo Monitor

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, traceback
 from flask import Flask, request, jsonify
 from extensions import db
 import models
@@ -12,7 +12,34 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 # Routes
-
+@app.route('/api/monitor/read_all', methods=['GET'])
+def read_all_monitors():
+  monitors = models.Monitor.query.all()
+  monitors_formatted = []
+  
+  try:
+    for monitor in monitors:
+      new_monitor = {
+        "name": monitor.name,
+        "url": monitor.url,
+        "frequency": monitor.frequency,
+        "status": monitor.status,
+        "description": monitor.description
+      }
+      monitors_formatted.append(new_monitor)
+    
+    return jsonify({
+      "message": "Todos os monitores carregados!",
+      "monitors": monitors_formatted
+    }),200
+    
+  except Exception as e:
+    full_traceback = traceback.format_exc()
+    print(f"Erro ao carregar os monitores! \n{e}")
+    return jsonify({
+      "message": "Erro ao salvar o monitor!",
+      "error": full_traceback
+    }), 400
 
 @app.route('/api/monitor/add', methods=['POST'])
 def add_monitor():
@@ -28,10 +55,46 @@ def add_monitor():
     
     db.session.add(new_monitor)
     db.session.commit()
-    return jsonify({ "message": "Dados salvos com sucesso!" }), 200
+    return jsonify({
+      "message": "Monitor salvo com sucesso!",
+      "monitor_id": new_monitor.id
+    }), 200
   
-  return jsonify({"message": "Dados do produto inválidos!"}), 400
+  return jsonify({"message": "Dados do produto inválidos ou faltantes!"}), 400
 
+@app.route('/api/monitor/update/<int:monitor_id>', methods=['PUT'])
+def update_monitor(monitor_id):
+  monitor = models.Monitor.query.get_or_404(monitor_id)
+  new_monitor = request.get_json()
+  
+  if 'name' in new_monitor:
+    monitor.name = new_monitor['name']
+  if 'url' in new_monitor:
+    monitor.url = new_monitor['url']
+  if 'frequency' in new_monitor:
+    monitor.frequency = new_monitor['frequency']
+  if 'status' in new_monitor:
+    monitor.status = new_monitor['status']
+  if 'description' in new_monitor:
+    monitor.description = new_monitor['description']
+    
+  db.session.commit()
+    
+  return jsonify({
+    "message": f"Monitor atualizado com sucesso!",
+    "new_monitor": new_monitor
+  }), 200
+
+@app.route('/api/monitor/delete/<int:monitor_id>', methods=['DELETE'])
+def delete_monitor(monitor_id):
+  monitor = models.Monitor.query.get_or_404(monitor_id)
+  db.session.delete(monitor)
+  db.session.commit()
+  
+  return jsonify({
+    "message": "Monitor deletado com sucesso!",
+    "monitor_id": monitor.id
+  }), 200
 
 # Config
 if __name__ == "__main__":
