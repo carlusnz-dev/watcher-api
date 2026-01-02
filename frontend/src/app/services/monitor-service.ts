2a41c9c64486aa077e460b3a7f27daeff90c0379
Carlos Antunes <carlosantunes.dev@gmail.com>
2026-01-02 11:06:21 -0300

    Atualização no services do Monitor

--- frontend/src/app/services/monitor-service.ts
+++ frontend/src/app/services/monitor-service.ts
@@ -23,4 +23,19 @@ export class MonitorService {
     const monitors = response.monitors;
     return monitors;
   }
+
+  async createNewMonitor(data: any): Promise<any> {
+    const response = await firstValueFrom(this.http.post<any>(this.baseUrl + '/add', data));
+    return response;
+  }
+
+  async updateMonitor(id: number, data: any): Promise<any> {
+    const response = await firstValueFrom(this.http.put<any>(this.baseUrl + `/update/${id}`, data));
+    return response;
+  }
+
+  async deleteMonitor(id: number): Promise<any> {
+    const response = await firstValueFrom(this.http.delete<any>(this.baseUrl + `/delete/${id}`));
+    return response;
+  }
 }
